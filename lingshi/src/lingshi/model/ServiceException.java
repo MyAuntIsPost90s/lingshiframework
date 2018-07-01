df14cca7d1d82bb3a667fd6f83b40dbb3db92855
MyAuntIsPost90s <1126670571@qq.com>
2018-07-01 12:19:20 +0800

    添加错误处理类ServiceException

--- lingshi/src/lingshi/model/ServiceException.java
+++ lingshi/src/lingshi/model/ServiceException.java
@@ -3,4 +3,17 @@ package lingshi.model;
 public class ServiceException extends Throwable {
 
 	private static final long serialVersionUID = -3405750937683912354L;
+
+	public ServiceException(String msg) {
+		super(msg);
+	}
+
+	public ServiceException(Exception e) {
+		super(e);
+	}
+
+	public ServiceException(String msg, Exception e) {
+		super(msg, e);
+	}
+
 }
