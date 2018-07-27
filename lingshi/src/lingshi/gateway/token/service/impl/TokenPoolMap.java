a4d3f958cec7f66e002beb4ae6a6dc1054c6822a
MyAuntIsPost90s <1126670571@qq.com>
2018-07-28 00:40:45 +0800

    修正重复登陆时报错

--- lingshi/src/lingshi/gateway/token/service/impl/TokenPoolMap.java
+++ lingshi/src/lingshi/gateway/token/service/impl/TokenPoolMap.java
@@ -51,6 +51,7 @@ public class TokenPoolMap extends TokenPoolBase {
 				UserToken item = (UserToken) entry.getValue();
 				if (item.getData().equals(userToken.getData())) {
 					delete(item.getToken());
+					break;
 				}
 			}
 		}
